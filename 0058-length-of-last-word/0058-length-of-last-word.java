dc0af6a3ab6821169983deca1392bfe8025ee009
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2025-03-22 20:49:46 +0530

    Update 0058-length-of-last-word.java

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -5,6 +5,7 @@ class Solution {
         return arr[arr.length-1].length(); 
     }
 }
-
+// In this question the s string is basically a sentence we just have to return the
+// length of last word 
 // first we have used trim to remove starting and ending extra space removing 
 // then we split the string then find out the length of last word 
