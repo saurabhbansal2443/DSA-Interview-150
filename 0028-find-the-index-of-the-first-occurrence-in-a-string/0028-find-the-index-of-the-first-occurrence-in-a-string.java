5892033010d0438d1b56c5b2d2782e96902f59e0
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2025-03-22 20:39:53 +0530

    Update 0028-find-the-index-of-the-first-occurrence-in-a-string.java

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -6,4 +6,6 @@ class Solution {
         return haystack.indexOf(needle);
 
     }
+    // in this question basically we have to see that the does the needle string 
+    // is present in haystack if present just return the first index 
 }
